19d234c478abf459a298ba7148d575f5fb19cdce
PolyakovVictor <polyakov.victor21@gmail.com>
2026-08-01 18:15:49 +0300

    feat: added mario rewards

--- main.py
+++ main.py
@@ -56,6 +56,42 @@ class ReplayBuffer:
     def __len__(self,): return len(self.buffer)
 
 
+class MarioEnv:
+    def __init__(self, rom_path='game_mario.gb'):
+        self.game = PyBoy(rom_path)
+        self.game.set_emulation_speed(0) # no speed limit
+        self.prior_x = 0
+
+    def get_mario_x(self,):
+        page = self.game.memory[0xC0AB]
+        offset = self.game.memory[0xC202]
+        return page * 256 + offset
+    
+    def is_dead(self,):
+        return self.game.memory[0xC0AC] == 0x06
+    
+    def step(self, action_idx, frame_skip=4):
+        apply_action(self.game, action_idx)
+
+        for _ in range(frame_skip):
+            self.game.tick()
+        
+        current_x = self.get_mario_x()
+        
+        reward = float(current_x - self.prior_x)
+        self.prior_x = current_x
+        
+
+        done = self.is_dead()
+        if done:
+            reward = -100.00
+        
+        reward -= 0.1
+
+        return reward, done
+        
+
+
 def preprocess_frame(raw_frame):
     rgb = raw_frame[:, :, :3]
     gray = np.dot(rgb, [0.299, 0.587, 0.144]).astype(np.uint8)
@@ -63,8 +99,8 @@ def preprocess_frame(raw_frame):
     return downsampled
 
 
-game = PyBoy('game_mario.gb')
-game.set_emulation_speed(0)
+mario = MarioEnv()
+game = mario.game
 
 buffer = ReplayBuffer(MAX_BUFF)
 frame_stack = deque(maxlen=4)
@@ -83,7 +119,8 @@ while step < 10000:
     state = np.array(frame_stack) # 4,72,80
 
     action = random.randint(0,4)
-    apply_action(game, action)
+    # apply_action(game, action)
+    mario.step(action)
     
     game.tick()
     next_frame = preprocess_frame(game.screen.ndarray)
